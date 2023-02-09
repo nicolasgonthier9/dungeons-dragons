948a21cbf3efef858ddf6fc13aedd36d8dc659ce
eizo <nicolasgonthier9@gmail.com>
2023-02-09 15:56:41 +0100

    Première itération terminée

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        Menu m = new Menu();
-        Character player = m.startMenu();
-        Game game = new Game(player);
-        game.run();
+        boolean gameState = true;
+        while (gameState) {
+            Menu m = new Menu();
+            Character player = m.startMenu();
+            Game game = new Game(player);
+            game.run();
+            gameState = game.restart();
+        }
     }
 }
